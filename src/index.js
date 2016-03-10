dab9a945a058c4522fa2b04c2051d9388814218b
xszhangfred <xszhang.fred@gmail.com>
2016-03-10 18:50:45 -0500

    webpack loader, es6, react component

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { App } from './App';
+
+import App from './App';
+import './style/main.scss';
 
 render(<App />, document.getElementById('root'));
