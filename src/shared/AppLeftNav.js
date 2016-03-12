3b28fbf0f20940074697dbaac06bf34acd6d7756
xszhangfred <xszhang.fred@gmail.com>
2016-03-12 01:03:48 -0500

    AppLeftNav Component

--- src/shared/AppLeftNav.js
+++ src/shared/AppLeftNav.js
@@ -1,18 +1,64 @@
 import React, { Component } from 'react';
 import LeftNav from 'material-ui/lib/left-nav';
 
+import List from 'material-ui/lib/lists/list';
+import ListItem from 'material-ui/lib/lists/list-item';
+import {SelectableContainerEnhance} from 'material-ui/lib/hoc/selectable-enhance';
+
+const SelectableList = SelectableContainerEnhance(List);
+
 class AppLeftNav extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {open: false};
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      open: false,
+      selectedIndex: '/home'
+    };
   }
 
   render() {
+    let styles = {
+      header: {
+        cursor: 'pointer',
+        fontSize: '24px',
+        color: '#fff',
+        lineHeight: '64px',
+        fontWeight: '300',
+        backgroundColor: '#00bcd4',
+        paddingLeft: '24px',
+        paddingTop: '0px',
+        marginBottom: '8px',
+      },
+      selectedList: {
+        color: '#ff4081',
+        backgroundColor: 'rgba(0, 0, 0, 0.03)',
+      }
+    };
+
     return (
       <LeftNav open={this.state.open}
                docked={false}
-               onRequestChange={open => this.setState({open})}
-               />
+               onRequestChange={open => this.setState({open})}>
+        <div style={styles.header}
+          onTouchTap={this.handleTouchTapHeader.bind(this)}>
+          S2
+        </div>
+        <SelectableList
+          selectedItemStyle={styles.selectedList}
+          valueLink={{
+            value: this.state.selectedIndex,
+            requestChange: this.handleUpdateSelectedIndex.bind(this), }}>
+          <ListItem
+            value="/home"
+            primaryText="Home" />
+          <ListItem
+            value="/account"
+            primaryText="Account" />
+          <ListItem
+            value="/about"
+            primaryText="About" />
+        </SelectableList>
+      </LeftNav>
     );
   }
 
@@ -20,6 +66,22 @@ class AppLeftNav extends Component {
     this.setState({open: !this.state.open});
   }
 
+  handleUpdateSelectedIndex(e, index) {
+    this.context.router.push(index);
+    this.setState({
+      open: false,
+      selectedIndex: index,
+    });
+  }
+
+  handleTouchTapHeader() {
+    this.context.router.push('/home');
+    this.setState({open: false});
+  }
 }
+
+AppLeftNav.contextTypes = {
+  router: React.PropTypes.object.isRequired
+};
 
 export default AppLeftNav;
