cf1e04b0c2b60bce06b17759f0b1b5777c12a114
xszhangfred <xszhang.fred@gmail.com>
2016-03-12 17:02:19 -0500

    Ajax, Github API

--- src/Account.js
+++ src/Account.js
@@ -2,14 +2,37 @@ import React, { Component } from 'react';
 import FlatButton from 'material-ui/lib/flat-button';
 import TextField from 'material-ui/lib/text-field';
 import Card from 'material-ui/lib/card/card';
+import axios from 'axios';
+import isEmpty from 'lodash/fp/isEmpty';
+import UserInfo from './user/UserInfo';
 
 class Account extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {},
+    };
+  }
   _handleSubmit(e) {
     e.preventDefault();
     const account = this.refs.account.getValue();
-    console.log(account);
+    axios.get(`https://api.github.com/users/${account}`)
+         .then((res) => {
+           this.setState({user: res.data});
+           console.log(res);
+         })
+        .catch((res) => {
+          console.log(res);
+        });
   }
   render() {
+    let GitHubInfo;
+
+    if(!isEmpty(this.state.user)) {
+      GitHubInfo = (
+        <UserInfo userInfo={this.state.user} />
+      );
+    }
     return (
       <div className='account'>
         <Card className="content">
@@ -21,6 +44,7 @@ class Account extends Component {
                         primary={true}/>
           </form>
         </Card>
+        { GitHubInfo }
       </div>
     );
   }
