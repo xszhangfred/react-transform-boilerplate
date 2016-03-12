b96b13ba07554a11d2ae036c9f953ae4f1f82966
xszhangfred <xszhang.fred@gmail.com>
2016-03-12 16:09:26 -0500

    React Form

--- src/Account.js
+++ src/Account.js
@@ -1,10 +1,26 @@
 import React, { Component } from 'react';
+import FlatButton from 'material-ui/lib/flat-button';
+import TextField from 'material-ui/lib/text-field';
+import Card from 'material-ui/lib/card/card';
 
 class Account extends Component {
+  _handleSubmit(e) {
+    e.preventDefault();
+    const account = this.refs.account.getValue();
+    console.log(account);
+  }
   render() {
     return (
       <div className='account'>
-        account page here
+        <Card className="content">
+          <form onSubmit={this._handleSubmit.bind(this)}>
+            <TextField hintText="Your Github Account"
+                       ref="account"/>
+            <FlatButton label="Search Github"
+                        type="submit"
+                        primary={true}/>
+          </form>
+        </Card>
       </div>
     );
   }
